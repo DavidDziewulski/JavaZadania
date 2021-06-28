51056ddee2dab22aa1e8855fd92f3f05ae071fab
Dawid <daviddziewulski@gmail.com>
2021-06-28 16:28:20 +0200

    Zadanie1

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,43 @@
 package com.company;
 
 public class Animal {
+    String name;
+    final String species;
+    Double weight;
+    static final public Double DEFAULT_ANIMAL_WEIGHT = 1.0;
+    public Animal(String species) {
+        this.species = species;
+
+        switch(this.species){
+            case "canis familiaris" :
+                this.weight = 10.0;
+                break;
+            case "feline" :
+                this.weight = 2.0;
+                break;
+            default: this.weight = DEFAULT_ANIMAL_WEIGHT;
+        }
+    }
+
+    public void feed(){
+        if(this.weight == 0.0){
+            System.out.println("Niestety zwięrze nie żyje nie możesz nakarmić nie żyjącego zwierzaka ? Bro!!");
+            return;
+        }
+        this.weight += 1.0;
+        System.out.println("Dziękuje za jedzenie");
+    }
+
+    public void takeForAWalk(){
+        if(this.weight == 0.0){
+            System.out.println("Niestety zwięrze nie żyje!");
+            return;
+        }else if(this.weight == 1.5){
+            System.out.println("To był jego ostatni spacer :( ");
+            this.weight -= 1.5;
+            return;
+        }
+        this.weight -= 1.5;
+        System.out.println("Dziękuje za spacer");
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Animal szarik = new Animal("feline");
+	szarik.name = "szarik";
+	System.out.println(szarik.weight); // Weight = 2.0
+	szarik.feed(); // Weight = 3.0
+	szarik.takeForAWalk(); // Weight = 3.0 - 1.5
+	szarik.takeForAWalk(); // Weight = 1.5 - 1.5
+	szarik.takeForAWalk(); // Weight =0.0
+	szarik.feed(); // Weight = 0.0
+
     }
 }
