ec7e23117adbe059743873b89988665ecc6253ca
Dawid <daviddziewulski@gmail.com>
2021-06-28 16:55:24 +0200

    Zadanie 3  4

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,5 +1,5 @@
 package com.company;
-
+import java.util.Date;
 public class Human {
     String firstName;
     String lastName;
@@ -8,8 +8,9 @@ public class Human {
     Phone mobilePhone;
     Animal pet;
     Car vehicle;
+    private Double salary;
 
-    public Human( String firstName, String lastName, String sex, int age, Phone mobilePhone, Animal pet){
+    public Human(String firstName, String lastName, String sex, Integer age, Phone mobilePhone, Animal pet) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.sex = sex;
@@ -18,6 +19,23 @@ public class Human {
         this.pet = pet;
     }
 
+    public double getSalary() {
+        Date date = new Date();
+        System.out.println(date); // Informacje o dacie
+        System.out.println(this.salary + "$"); //Informacje ile było kasy
+        return this.salary; // Zwraca zarobki
+    }
+
+    public void setSalary(Double new_sallary) {
+        if (new_sallary < 0.0) {
+            System.out.println("Chcesz zarabiać mniej niż 0$?");
+        } else {
+            System.out.println("Nowe dane zostały wysłane do systemu księgowego");
+            System.out.println("Odbierz aneks do umowy od pani Hani");
+            System.out.println("Zus i Us już wiedzą o zmianie wypłaty");
+            this.salary = new_sallary;
+        }
 
 
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,24 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    //Task1
-//	Animal szarik = new Animal("feline");
-//	szarik.name = "szarik";
-//	System.out.println(szarik.weight); // Weight = 2.0
-//	szarik.feed(); // Weight = 3.0
-//	szarik.takeForAWalk(); // Weight = 3.0 - 1.5
-//	szarik.takeForAWalk(); // Weight = 1.5 - 1.5
-//	szarik.takeForAWalk(); // Weight =0.0
-//	szarik.feed(); // Weight = 0.0
-
-
-	// Task 2
-
+	//Task 3
 	Phone iphone = new Phone("Iphone","Se","Ios",5.6);
 	Car audi = new Car("Audi","A3","1.9Tdi","smkdsk1024124");
 	Animal szarik = new Animal("feline");
 	Human dawid = new Human("Dawid","Dziewulski","male",25,iphone,szarik);
-	dawid.vehicle = audi;
-	System.out.println(dawid.vehicle);
+	dawid.setSalary(2000.0);
+	dawid.getSalary();
     }
 }
